7b931a6693615ac4f313e2978a7f01ea2e0cf7bf
Doğuhan İlter <doguhannilt@gmail.com>
2024-04-06 10:37:03 +0300

    ProtectedAuth is added for /user-profile endpoint

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Layout from './layouts/layout'
 import HomePage from './pages/HomePage'
 import EditProfile from './components/EditProfile'
 import { useAuth0 } from '@auth0/auth0-react'
+import ProtectedAuth from './auth/ProtectedAuth'
 
 
 const AppRoutes = () => {
@@ -10,7 +11,10 @@ const AppRoutes = () => {
     return(
         <Routes>
             <Route  path = "/" element={<Layout><HomePage /></Layout>}/>
-            <Route  path = {`/user-profile/:email`} element={<Layout><EditProfile/></Layout>}/>
+            <Route element={<ProtectedAuth/>}>
+                <Route  path = {`/user-profile/:email`} element={<Layout><EditProfile/></Layout>}/>
+            </Route>
+            
             <Route  path = "*" element={<Navigate to="/" />}/>
         </Routes>
     )
---
+++ frontend/src/auth/ProtectedAuth.jsx
@@ -0,0 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const ProtectedAuth = () => {
+    const {isAuthenticated} = useAuth0
+    return isAuthenticated ? (<Outlet/>) : (<Navigate to="/" replace/>)
+}
+
+export default ProtectedAuth
